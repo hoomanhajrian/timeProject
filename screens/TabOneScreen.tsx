7abd8fcd943a508f7378f6e4f79ba1dc4e771969
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2022-02-26 18:16:09 -0800

    added flatlist for displaying items in first tab

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,33 +1,41 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+import { StyleSheet, FlatList,Image } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { Card,CardHeader } from '@mui/material';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+const [dataSource, changeDataSource] = useState([{id:1,title:"title"},{id:2,title:"title2"}]);
+const [loading, changeLoading]= useState([true]);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <Text>This is tab oneD</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+  <SafeAreaView>
+    {loading?
+    <Text>loading</Text>
+    :<FlatList 
+    style={styles.container} 
+    data={dataSource}
+    renderItem={({item})=>(
+      <Card>
+        <CardHeader
+        title={item.title}
+        />
+      </Card>
+    )}
+    />}
+  </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: 'flex',
+    flexDirection:'column',
+
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
